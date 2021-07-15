3d6d3f37eaf27ad95ddcac8da867bee3da7ae99c
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-16 00:36:24 +0200

    Bump rtl8189fs to accomodate recent kernels

--- package/rtl8189fs/rtl8189fs.mk
+++ package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 54bd6808e38f213126e7d6447ae9d80c656179f3
+RTL8189FS_VERSION = 03ac413135a355b55b693154c44b70f86a39732e
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
