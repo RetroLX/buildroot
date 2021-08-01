f04cf9116afc0a2548ccd2d86326ec1a45f2e2eb
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-01 16:31:28 +0200

    Bump poppler to 21.07 to avoid glibc 2.33 issues

--- package/poppler/poppler.mk
+++ package/poppler/poppler.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POPPLER_VERSION = 0.84.0
+POPPLER_VERSION = 21.07.0
 POPPLER_SOURCE = poppler-$(POPPLER_VERSION).tar.xz
 POPPLER_SITE = http://poppler.freedesktop.org
 POPPLER_DEPENDENCIES = fontconfig host-pkgconf
