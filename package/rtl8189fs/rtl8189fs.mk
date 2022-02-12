060a7072ddaf4e90c5618ea69d527a241029ef9b
Romain TISSERAND <romain.tisserand@gmail.com>
2022-02-12 08:09:31 +0100

    rtl8189fs fix on 5.15 kernel

--- package/rtl8189fs/rtl8189fs.mk
+++ package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 03ac413135a355b55b693154c44b70f86a39732e
+RTL8189FS_VERSION = be378f47055da1bae42ff6ec1d62f1a5052ef097
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
