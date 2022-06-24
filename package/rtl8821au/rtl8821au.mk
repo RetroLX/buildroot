e181bceacc22aebeca1e0c4aa10305e73a7b10e9
Romain TISSERAND <romain.tisserand@gmail.com>
2022-06-24 11:19:14 +0200

    Bump rtl8821au

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = e2df993bfd5b88e61770b461d94d0700cec2dbe5
+RTL8821AU_VERSION = e0b443940471c166a5cc6280d3608f95228e017f
 RTL8821AU_SITE = $(call github,lwfinger,rtl8812au,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPL-2.0
 
