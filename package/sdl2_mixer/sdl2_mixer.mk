e84ebc1927eb9294e12aec7ca423ac8f6fa08597
Romain TISSERAND <romain.tisserand@gmail.com>
2022-07-19 14:20:48 +0200

    SDL2_mixer 2.6.1

--- package/sdl2_mixer/sdl2_mixer.mk
+++ package/sdl2_mixer/sdl2_mixer.mk
@@ -4,58 +4,65 @@
 #
 ################################################################################
 
-SDL2_MIXER_VERSION = 2.0.4
+SDL2_MIXER_VERSION = 2.6.1
 SDL2_MIXER_SOURCE = SDL2_mixer-$(SDL2_MIXER_VERSION).tar.gz
-SDL2_MIXER_SITE = http://www.libsdl.org/projects/SDL_mixer/release
+SDL2_MIXER_SITE =  https://github.com/libsdl-org/SDL_mixer/releases/download/release-$(SDL2_MIXER_VERSION)
 SDL2_MIXER_LICENSE = Zlib
 SDL2_MIXER_LICENSE_FILES = COPYING.txt
 SDL2_MIXER_INSTALL_STAGING = YES
 SDL2_MIXER_DEPENDENCIES = sdl2 host-pkgconf
 
-ifeq ($(BR2_PACKAGE_FLAC),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-flac
-SDL2_MIXER_DEPENDENCIES += flac
+SDL2_MIXER_SUPPORTS_IN_SOURCE_BUILD = NO
+
+ifeq ($(BR2_PACKAGE_LIBXMP),y)
+SDL_MIXER_CONF_OPTS += -DSDL2MIXER_MOD_XMP=ON -DSDL2MIXER_MOD_XMP_SHARED=ON
+SDL2_MIXER_DEPENDENCIES += libxmp
 else
-SDL2_MIXER_CONF_OPTS += --disable-music-flac
+SDL_MIXER_CONF_OPTS += -DSDL2MIXER_MOD_XMP=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_FLUIDSYNTH)$(BR2_PACKAGE_TIMIDITY),)
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_MIDI=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_FLUIDSYNTH),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-midi-fluidsynth
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_MIDI_FLUIDSYNTH=ON -DSDL2MIXER_MIDI_FLUIDSYNTH_SHARED=ON
 SDL2_MIXER_DEPENDENCIES += fluidsynth
 else
-SDL2_MIXER_CONF_OPTS += --disable-music-midi-fluidsynth
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_MIDI_FLUIDSYNTH=OFF
 endif
 
-ifeq ($(BR2_PACKAGE_LIBMODPLUG),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-mod-modplug
-SDL2_MIXER_DEPENDENCIES += libmodplug
+ifeq ($(BR2_PACKAGE_TIMIDITY),y)
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_MIDI_TIMIDITY=ON
+SDL2_MIXER_DEPENDENCIES += timidity
 else
-SDL2_MIXER_CONF_OPTS += --disable-music-mod-modplug
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_MIDI_TIMIDITY=OFF
 endif
 
-ifeq ($(BR2_PACKAGE_OPUSFILE),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-opus
-SDL2_MIXER_DEPENDENCIES += opusfile
-else
-SDL2_MIXER_CONF_OPTS += --disable-music-opus
+ifeq ($(BR_PACKAGE_LIBOGG)$(BR2_PACKAGE_OPUS)$(BR2_PACKAGE_OPUSFILE),yyy)
+SDL2_MIXER_CONF_OPTS += -DSDL2MIXER_OPUS_SHARED=ON
+SDL2_MIXER_DEPENDENCIES += libogg opus libopusfile
 endif
 
-ifeq ($(BR2_PACKAGE_TREMOR),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-ogg-tremor
-SDL2_MIXER_DEPENDENCIES += tremor
-else
-SDL2_MIXER_CONF_OPTS += --disable-music-ogg-tremor
-endif
+#ifeq ($(BR2_PACKAGE_SDL2_MIXER_USE_LIBFLAC),y)
+#SDL2_MIXER_CONF_OPTS += --enable-music-flac
+#SDL2_MIXER_DEPENDENCIES += flac
+#else
+#SDL2_MIXER_CONF_OPTS += --disable-music-flac
+#endif
 
-# batocera
-ifeq ($(BR2_PACKAGE_LIBMAD),y)
-SDL2_MIXER_CONF_OPTS += --enable-music-mp3-mad-gpl
-SDL2_MIXER_DEPENDENCIES += libmad
-endif
+#ifeq ($(BR2_PACKAGE_SDL2_MIXER_USE_LIBMODPLUG),y)
+#SDL2_MIXER_CONF_OPTS += --enable-music-mod-modplug
+#SDL2_MIXER_DEPENDENCIES += libmodplug
+#else
+#SDL2_MIXER_CONF_OPTS += --disable-music-mod-modplug
+#endif
 
-# batocera
-ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
-SDL2_MIXER_DEPENDENCIES += libvorbis
-endif
+#ifeq ($(BR2_PACKAGE_TREMOR),y)
+#SDL2_MIXER_CONF_OPTS += --enable-music-ogg-tremor
+#SDL2_MIXER_DEPENDENCIES += tremor
+#else
+#SDL2_MIXER_CONF_OPTS += --disable-music-ogg-tremor
+#endif
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
